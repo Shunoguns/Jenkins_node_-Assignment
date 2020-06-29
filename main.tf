85ee78d3e7b2a4500074a1f38e42f52e6dc99f48
Seun <shunoguns@yahoo.com>
2020-06-29 01:41:07 +0100

    fully updated version

--- main.tf
+++ main.tf
@@ -4,25 +4,37 @@ public_key      = file(var.path_to_public_key)
 }
 
 resource "aws_instance" "Jenkins_node" {
-    ami           = "ami-0ea3405d2d2522162"
+    ami           = "ami-031a03cb800ecb0d5"
     instance_type = "t2.micro"
     key_name      = "junekey"
     subnet_id     = var.subnet_id
-
-
+  
     tags          = {
     Name          = var.tag
   }
 
-provisioner "remote-exec"  {
-  inline = [
-      "sudo yum install git -y" , 
-      "sudo yum install maven -y",
-      "sudo yum install tomcat -y",
-      "sudo yum install jenkins -y",
-      ]
-    
+provisioner "remote-exec" {
+    connection {
+    type = "ssh"
+    user = "ec2-user"
+    host = aws_instance.Jenkins_node.public_ip
+    private_key = file(var.path_to_private_key)
   }
+    inline = [
+      "sudo yum update -y",
+      "sudo yum -y install nginx",
+      "sudo service nginx start",
+      "sudo yum install maven git -y",
+      "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo",
+      "sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key",
+      "sudo yum install jenkins -y",
 
+    ]
+  }
+}
+       
+    
 
+output "ip" {
+  value = "https://${aws_instance.Jenkins_node.public_ip}:8080  connect ssh -i junekey ec2-user@${aws_instance.Jenkins_node.public_ip}"
 }
